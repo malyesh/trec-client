f5a17347a5a07e69c10b14c0a47e54e279ed5aaf
malyesh <74512928+malyesh@users.noreply.github.com>
2023-12-13 13:08:11 -0500

    make list page adaptable to any search from new searchpage

--- src/pages/LandmarkListPage/LandmarkListPage.js
+++ src/pages/LandmarkListPage/LandmarkListPage.js
@@ -5,10 +5,14 @@ import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './LandmarkListPage.scss';
+import Input from '../../components/Input/Input';
+import sadIcon from '../../assets/icons/sad-face-in-rounded-square-svgrepo-com.svg';
 
 export default function LandmarkListPage() {
   const [landmarkList, setLandmarkList] = useState(null);
   const [selectedLandmark, setSelectedLandmark] = useState();
+  // const [search, setSearch] = useState('');
+
   const [allLandmarks, setAllLandmarks] = useState();
   const { query } = useParams();
   const location = useLocation();
@@ -19,7 +23,17 @@ export default function LandmarkListPage() {
 
   // console.log(state.landmarks);
   useEffect(() => {
-    setAllLandmarks(state.landmarks);
+    setAllLandmarks(
+      state.landmarks.map((obj) => ({
+        value: obj.landmark_name,
+        label: obj.landmark_name,
+        city_id: obj.city_id,
+        country_id: obj.country_id,
+        country_name: obj.country_name,
+        city_name: obj.city_name,
+        id: obj.id,
+      }))
+    );
   }, [state]);
 
   // useEffect(() => {
@@ -43,16 +57,16 @@ export default function LandmarkListPage() {
   //   getLandmarkList();
   // }, [apiBody, countryId, cityId]);
 
-  // useEffect(() => {
-  //   if (selectedLandmark) {
-  //     navigate(
-  //       `/${selectedLandmark.country_id}/${selectedLandmark.city_id}/${selectedLandmark.id}`,
-  //       {
-  //         state: { name: selectedLandmark.landmark_name },
-  //       }
-  //     );
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (selectedLandmark) {
+      navigate(
+        `/${selectedLandmark.country_id}/${selectedLandmark.city_id}/${selectedLandmark.id}`,
+        {
+          state: { name: selectedLandmark.label },
+        }
+      );
+    }
+  }, [selectedLandmark]);
 
   if (!allLandmarks) return <h1>Loading...</h1>;
   // console.log(allLandmarks);
@@ -62,28 +76,47 @@ export default function LandmarkListPage() {
       <Title title={`Landmark results for "${query}"`} />
 
       <main className='list__body'>
-        {/* <Dropdown
+        <Dropdown
           options={allLandmarks}
           setSelectedElement={setSelectedLandmark}
           value={selectedLandmark}
           name='landmark'
           id='landmark'
           type='landmark'
-        /> */}
+        />
 
         <section className='list__container'>
-          {allLandmarks.map((landmark, i) => {
-            return (
-              <Landmark
-                key={i}
-                // id={landmark.id}
-                // name={landmark.landmark_name}
-                // country={landmark.country_name}
-                // city={landmark.city_name}
-                landmark={landmark}
-              />
-            );
-          })}
+          {allLandmarks.length > 0 ? (
+            allLandmarks.map((landmark, i) => {
+              return (
+                <Landmark
+                  key={i}
+                  // id={landmark.id}
+                  // name={landmark.landmark_name}
+                  // country={landmark.country_name}
+                  // city={landmark.city_name}
+                  landmark={landmark}
+                />
+              );
+            })
+          ) : (
+            <div className='list__empty'>
+              <h2 className='list__empty--title'>
+                No landmarks for your search yet, please be patient with us!
+              </h2>
+              <img className='list__empty--icon' src={sadIcon} alt='sad face' />
+              {/* <form className='list__form'>
+                  <Input
+                    label=''
+                    name='search'
+                    type='text'
+                    value={search}
+                    onChange={handleInputChange}
+                    placeholder='Search...'
+                  />
+                </form> */}
+            </div>
+          )}
         </section>
       </main>
     </div>
